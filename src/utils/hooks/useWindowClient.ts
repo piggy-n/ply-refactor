4eaa8a4574f5d3e7dd51425195e3fe001534bdee
lzn01 <408135242@qq.com>
2022-10-15 09:17:47 +0800

    useWindowClient hooks

---
+++ src/utils/hooks/useWindowClient.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+export interface useWindowSizeType {
+    clientX: number;
+    clientY: number;
+}
+
+const useWindowClient = (): useWindowSizeType => {
+    const [windowDistance, setWindowDistance] = useState<useWindowSizeType>({
+        clientX: 0,
+        clientY: 0,
+    });
+
+    useEffect(
+        () => {
+            function handle(e: MouseEvent) {
+                setWindowDistance(() => ({
+                        clientX: e.clientX,
+                        clientY: e.clientY,
+                    })
+                );
+            }
+
+            addEventListener('mousemove', handle);
+
+            return () => removeEventListener('mousemove', handle);
+        },
+        []
+    );
+
+    return { ...windowDistance };
+};
+
+export default useWindowClient;
