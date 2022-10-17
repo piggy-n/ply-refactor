2301da5eab96b69023157954f4c940279252b161
lzn01 <408135242@qq.com>
2022-10-17 10:10:02 +0800

    container 添加最小宽高

--- src/core/RndPlayer/RndPlayer.tsx
+++ src/core/RndPlayer/RndPlayer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Rnd } from 'react-rnd';
 import type { FC } from 'react';
-import type { RndPlayerProps, DefaultPositionAndSize } from '@/index.d';
+import type { RndPlayerProps } from '@/index.d';
 import { classes } from '@/utils/methods/classes';
 import '@/assets/styles/global.scss';
 import { DEFAULT_PLAYER_OPTS } from '@/core/RndPlayer/config';
@@ -18,21 +18,28 @@ const RndPlayer: FC<RndPlayerProps> = (
     const {
         bounds = DEFAULT_PLAYER_OPTS.bounds,
         defaultSize = DEFAULT_PLAYER_OPTS.defaultSize,
-        defaultPosition = DEFAULT_PLAYER_OPTS.defaultPosition,
+        // defaultPosition = DEFAULT_PLAYER_OPTS.defaultPosition,
         style,
     } = playerOpts;
 
     return (
         <Rnd
             className={classes(cn, '')}
-            default={{ ...defaultSize, ...defaultPosition } as DefaultPositionAndSize}
             bounds={bounds}
             style={style}
             maxWidth={innerWidth}
             maxHeight={innerHeight}
+            minHeight={defaultSize!.height}
+            minWidth={defaultSize!.width}
             lockAspectRatio
         >
-            {renderHeader(header)}
+            <div className={classes(
+                cn,
+                'container',
+                [`mw-${defaultSize!.width}`, `mh-${defaultSize!.height}`]
+            )}>
+                {renderHeader(header)}
+            </div>
         </Rnd>
     );
 };
