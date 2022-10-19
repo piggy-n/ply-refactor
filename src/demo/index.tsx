93782c4d7dea6a924a6ccc59c72e2d95fff0178e
lzn01 <408135242@qq.com>
2022-10-19 16:30:26 +0800

    视频播放 url

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 // import RndPlayer from '@/core/RndPlayer';
 import Player from '@/core/Player';
-import { randomString } from '@/utils/methods/randomString';
+// import { randomString } from '@/utils/methods/randomString';
 
 const Demo = () => {
     const [url, setUrl] = React.useState('https://www.w3schools.com/html/mov_bbb.mp4');
     return (
         <>
-            <button onClick={() => setUrl(`ws://` + randomString())}>
+            <button onClick={() => setUrl('https://gs-files.oss-cn-hongkong.aliyuncs.com/okr/prod/file/2021/08/31/540p.mp4')}>
                 设url
             </button>
             <div style={{
