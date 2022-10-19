bb23bb884651c33d6134580c79f1ac89a3f8e828
lzn01 <408135242@qq.com>
2022-10-19 10:01:58 +0800

    eslintrc setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
         'node': true,
         'es2021': true,
     },
+    'settings': {
+        'react': {
+            'version': 'detect',
+        }
+    },
     'extends': [
         'eslint:recommended',
         'plugin:react/recommended',
