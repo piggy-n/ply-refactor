bf111ff95aa28ab3c29d8a6e01c704c1c2e11769
lzn01 <408135242@qq.com>
2022-11-03 14:48:21 +0800

    重构player

--- src/kernel/Player/Player.tsx
+++ src/kernel/Player/Player.tsx
@@ -34,11 +34,6 @@ const VanillaPlayer: ForwardRefRenderFunction<PlayerRef, PlayerProps> = (
     const { playerStore, playerStoreDispatch } = usePlayerStore();
     const { setState } = useRndPlayerStore;
 
-    const uuid = useMemo(
-        () => randomString(),
-        []
-    );
-
     const playerContextValue = useMemo(
         () => Object.assign(
             {},
@@ -46,17 +41,16 @@ const VanillaPlayer: ForwardRefRenderFunction<PlayerRef, PlayerProps> = (
                 ...playerContextDefaultValue,
                 playerStore,
                 playerStoreDispatch,
-                uuid,
                 videoProperties,
                 videoEle: videoEleRef.current,
                 videoContainerEle: videoContainerEleRef.current,
+                uuid: useMemo(() => randomString(), []),
                 ...rest
             }
         ),
         [
             playerStore,
             playerStoreDispatch,
-            uuid,
             videoEleRef.current,
             videoContainerEleRef.current,
             { ...rest }
@@ -81,7 +75,7 @@ const VanillaPlayer: ForwardRefRenderFunction<PlayerRef, PlayerProps> = (
         <PlayerContext.Provider value={playerContextValue}>
             <div
                 ref={videoContainerEleRef}
-                id={`${cnPrefix}-container-${uuid}`}
+                id={`${cnPrefix}-container-${playerContextValue.uuid}`}
                 className={classes(cn, '')}
                 onMouseOver={() => setState({ disableDrag: true })}
                 {...videoContainerEleOpts}
