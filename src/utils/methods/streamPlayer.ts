8c4a9e1d98b680dbf1be0ad05083071a55d95acf
lzn01 <408135242@qq.com>
2022-11-07 10:18:04 +0800

    StreamPlayer

--- src/utils/methods/streamPlayer.ts
+++ src/utils/methods/streamPlayer.ts
@@ -1,4 +1,39 @@
+// import MP4Box from 'mp4box';
+import type { Dispatch } from 'react';
+import type { PlayerStoreState } from '@/store/usePlayerStore';
+
+interface Options {
+    dispatch: Dispatch<PlayerStoreState>;
+}
+
 export class StreamPlayer {
     ws?: WebSocket;
     ele?: HTMLVideoElement;
+    url?: string;
+    mime?: string;
+    streaming?: boolean;
+    mediaSource?: MediaSource;
+    arrayBuffer: ArrayBuffer[];
+    sourceBuffer?: SourceBuffer;
+    MP4BoxFile?: any;
+    connectionTimes: number;
+    transmissionRate: number;
+
+    loadHandler?: () => void;
+    sourceOpenHandler?: () => void;
+    dispatch: Dispatch<PlayerStoreState>;
+    transmissionRateInterval?: NodeJS.Timeout;
+
+    constructor(options: Options) {
+        this.dispatch = options.dispatch;
+        this.arrayBuffer = [];
+        this.connectionTimes = 0;
+        this.transmissionRate = 0;
+    }
+
+    bindFunc(obj: Record<string, unknown>, func: () => void) {
+        return function () {
+            func.apply(obj, arguments);
+        };
+    }
 }
